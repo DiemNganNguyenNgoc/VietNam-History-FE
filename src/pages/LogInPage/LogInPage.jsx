d2a3b7128d03c56422d8f3d4527d1b5a3799ed86
HoaiNhu <22521051@gm.uit.edu.vn>
2024-11-02 21:20:17 +0700

    login

--- src/pages/LogInPage/LogInPage.jsx
+++ src/pages/LogInPage/LogInPage.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
+import FormComponent from "../../components/FormComponent/FormComponent";
 
-export default function LogInPage() {
+const LogInPage = () => {
   return (
-    <div>LogInPage</div>
-  )
-}
+    <div>
+      <h1 className="login__title">LOG IN</h1>
+      <FormComponent
+        id="emailInput"
+        label="Email"
+        type="email"
+        placeholder="Enter your email"
+      ></FormComponent>
+
+      <FormComponent
+        id="passwordInput"
+        label="Password"
+        type="password"
+        placeholder="Enter your password"
+      ></FormComponent>
+
+
+      <ButtonComponent>Log In</ButtonComponent>
+    </div>
+  );
+};
+
+export default LogInPage;
