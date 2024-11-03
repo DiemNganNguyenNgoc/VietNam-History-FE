86408f6cc985678e9fd8968c6756de94aab8816a
DiemNganNguyenNgoc <22520933@gm.uit.edu.vn>
2024-11-03 22:34:13 +0700

    slider_homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import SliderComponent from '../../components/SliderComponent/SliderComponent'
+import slider1 from '../../assets/image/slider1.webp'
+import slider2 from '../../assets/image/slider2.webp'
+import slider3 from '../../assets/image/slider3.webp'
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <SliderComponent arrImg = {[slider1 , slider2, slider3]}/>
+    </div>
   )
 }
 
