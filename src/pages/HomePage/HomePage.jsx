92c130b0b95f2741f6981ab59fef8dc766c6dc3f
DiemNganNguyenNgoc <22520933@gm.uit.edu.vn>
2024-11-03 23:23:43 +0700

    askquestion_Bnt

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import SliderComponent from '../../components/SliderComponent/SliderComponent'
-import slider1 from '../../assets/image/slider1.webp'
-import slider2 from '../../assets/image/slider2.webp'
-import slider3 from '../../assets/image/slider3.webp'
+import React from 'react';
+import SliderComponent from '../../components/SliderComponent/SliderComponent';
+import AskQuestionBnt from '../../components/AskQuestionBtn/AskQuestionBntOverride'
+import slider1 from '../../assets/image/slider1.webp';
+import slider2 from '../../assets/image/slider2.webp';
+import slider3 from '../../assets/image/slider3.webp';
 const HomePage = () => {
   return (
     <div>
+      <div>
       <SliderComponent arrImg = {[slider1 , slider2, slider3]}/>
+      </div>
+      <br></br>
+      <div className='title'>TOP QUESTION MAY INTEREST YOU</div>
+      <AskQuestionBnt/>
     </div>
   )
 }
