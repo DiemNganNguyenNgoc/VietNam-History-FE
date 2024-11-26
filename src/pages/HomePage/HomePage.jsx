0cd9bc8626a547f4ad0b68ec9090a6d8a2c46046
DiemNganNguyenNgoc <22520933@gm.uit.edu.vn>
2024-11-26 16:08:55 +0700

    commit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ import slider1 from '../../assets/image/slider1.webp';
 import slider2 from '../../assets/image/slider2.webp';
 import slider3 from '../../assets/image/slider3.webp';
 import QuestionHolder from '../../components/QuestionHolder/QuestionHolder';
+import SortBtnHome from '../../components/SortBtnHome/SortBtnHome'
 function HomePage() {
   const [activeTab, setActiveTab] = useState("interesting");
   return (
@@ -15,44 +16,11 @@ function HomePage() {
         <SliderComponent arrImg={[slider1, slider2, slider3]} />
       </div>
       <br></br>
-      <div className='title'>TOP QUESTION MAY INTEREST YOU</div>
-      <AskQuestionBnt />
-      <ul className="nav nav-tabs">
-
-        <li className="nav-item">
-          <button
-            className={`nav-link ${activeTab === "interesting" ? "active" : ""}`}
-            onClick={() => setActiveTab("interesting")}
-          >
-            Interesting
-          </button>
-        </li>
-        <li className="nav-item">
-          <button
-            className={`nav-link ${activeTab === "hot" ? "active" : ""}`}
-            onClick={() => setActiveTab("hot")}
-          >
-            Hot
-          </button>
-        </li>
-        <li className="nav-item">
-          <button
-            className={`nav-link ${activeTab === "week" ? "active" : ""}`}
-            onClick={() => setActiveTab("week")}
-          >
-            Week
-          </button>
-        </li>
-        <li className="nav-item">
-          <button
-            className={`nav-link ${activeTab === "month" ? "active" : ""}`}
-            onClick={() => setActiveTab("month")}
-          >
-            Month
-          </button>
-        </li>
-      </ul>
-
+      <div className="col-12">
+      <span className='title'>TOP QUESTION MAY INTEREST YOU</span>
+      <span><AskQuestionBnt/></span>
+      </div>
+     <SortBtnHome></SortBtnHome>
       <div className="row mt-4">
         <div className="col-12">
           {activeTab === "interesting" && <QuestionHolder />}
