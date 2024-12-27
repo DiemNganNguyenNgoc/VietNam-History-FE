5a5092758354a44836352141ca1e97e0bc9d4c7e
vhq3404 <22521195@gm.uit.edu.vn>
2024-12-27 16:01:32 +0700

    fix admin profile

---
+++ src/AdminPage/ProfileAdmin/ManageSystem.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TagAdmin from "../TagAdmin/TagAdmin";
+import UsersAdmin from "../UsersAdmin/UsersAdmin";
+import QuestionAdmin from "../QuestionAdmin/QuestionAdmin";
+import { useSelector } from "react-redux";
+
+function ManageSystem() {
+  const admin = useSelector((state) => state.admin);
+  const [activeTab, setActiveTab] = useState("question");
+
+  return (
+    <div className="container mt-4">
+      {/* Tabs */}
+      <div className="row mt-4">
+        <div className="col-12">
+          <ul className="nav nav-tabs">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${activeTab === "question" ? "active" : ""}`}
+                onClick={() => setActiveTab("question")}
+              >
+                Questions
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${activeTab === "tag" ? "active" : ""}`}
+                onClick={() => setActiveTab("tag")}
+              >
+                Tags
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${activeTab === "user" ? "active" : ""}`}
+                onClick={() => setActiveTab("user")}
+              >
+                Users
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Nội dung Tab */}
+      <div className="row mt-4">
+        <div className="col-12">
+          {activeTab === "question" && <QuestionAdmin />}
+          {activeTab === "tag" && <TagAdmin />}
+          {activeTab === "user" && <UsersAdmin />}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ManageSystem;
